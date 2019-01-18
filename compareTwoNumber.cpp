75e712698f6db60ac640016de8ead5f0eb2f9d7e
Peggy_Cao <yuqi.cao.sjtu@gmail.com>
2019-01-17 20:47:20 -0500

    practice c++ iostream

---
+++ compareTwoNumber.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int a,b;
+    cout << "Plz enter a number: ";
+    cin >> a;
+    //cout << endl;
+    cout << "Plz enter the second one: ";
+    cin >> b;
+
+    if (a > b) {
+        cout << "the larger one is: " << a << endl;
+    } else if (b > a) {
+        cout << "the larger one is: " << b << endl;
+    } else {
+        cout << "they are equal.";
+    }
+
+    return 0;
+}
